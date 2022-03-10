80a0dfe6739e4db1876da3185982c47e44af1f35
Andre Muniz <andremunizti@gmail.com>
2022-03-10 15:40:14 -0300

    Movie Card

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,20 @@
+import {ReactComponent as StarFull} from 'assets/img/star1.svg';
+import {ReactComponent as StarEmpty} from 'assets/img/star2.svg';
+import {ReactComponent as StarHalf} from 'assets/img/star3.svg';
+import './styles.css';
+
+function MovieStars() {
+
+    return (
+        <div className="dsmovie-stars-container">
+            <StarFull />
+            <StarFull />
+            <StarFull />
+            <StarHalf />
+            <StarEmpty />
+        </div>
+
+    );
+}
+
+export default MovieStars;
